ca34affd22fbe0bef1e5e1852324056bca73f0e6
sielskibartosz <sielskibartosz95@gmail.com>
2025-12-02 15:11:58 +0100

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import About from "./pages/About";
-import Exercises from "./pages/Exercises";
-import ExerciseCategory from "./pages/ExerciseCategory";
-import Pricing from "./pages/Pricing";
-import Contact from "./pages/Contact";
 import { LanguageProvider } from "./i18n/LanguageContext";
+import { Suspense, lazy } from "react";
+
+// Lazy loading podstron
+const About = lazy(() => import("./pages/About"));
+const Exercises = lazy(() => import("./pages/Exercises"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Contact = lazy(() => import("./pages/Contact"));
+const ExerciseCategory = lazy(() => import("./pages/ExerciseCategory"));
 
 function App() {
   return (
     <LanguageProvider>
       <Navbar />
 
-      <Routes>
-        {/* Strona główna */}
-        <Route
-          index
-          element={
-            <>
-              <About />
-              <Exercises />
-              <Pricing />
-              <Contact />
-            </>
-          }
-        />
+      {/* Suspense pokazuje fallback podczas ładowania */}
+      <Suspense fallback={<div style={{ textAlign: "center", marginTop: "100px", color: "#fff" }}>Ładowanie...</div>}>
+        <Routes>
+          {/* Strona główna */}
+          <Route
+            index
+            element={
+              <>
+                <About />
+                <Exercises />
+                <Pricing />
+                <Contact />
+              </>
+            }
+          />
 
-        {/* Podstrony ćwiczeń */}
-        <Route path="exercises/:category" element={<ExerciseCategory />} />
-      </Routes>
+          {/* Podstrony ćwiczeń */}
+          <Route path="exercises/:category" element={<ExerciseCategory />} />
+        </Routes>
+      </Suspense>
     </LanguageProvider>
   );
 }
